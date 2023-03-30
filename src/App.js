7612e3cd68d625b6994e00f095bddb93e3471bc1
siennalind <sienna.lebeaulind@gmail.com>
2023-03-30 18:32:38 +0100

    add basic header & nav components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Header from "./components/layout/Header"
+
 function App() {
   return (
-    <></>
+    <Header />
   )
 }
 
---
+++ src/components/layout/Nav.js
@@ -0,0 +1,11 @@
+function Nav () {
+    return (
+        <nav>
+            <ul>
+                <li>Menu Item</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
