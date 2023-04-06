9e35ba55dae1f7dda7ca8361c5c20d35d87a6059
siennalind <sienna.lebeaulind@gmail.com>
2023-04-06 19:36:30 +0100

    added a search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,11 @@
+function Search () {
+    return (
+        <form>
+            <label>Search
+            <input type="search" />
+            </label>
+        </form>
+    )
+}
+
+export default Search
