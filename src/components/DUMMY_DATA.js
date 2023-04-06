186c6233ef3f2a9952a78ba35538abbba46ed821
siennalind <sienna.lebeaulind@gmail.com>
2023-04-06 19:35:56 +0100

    added dummy data

---
+++ src/components/DUMMY_DATA.js
@@ -0,0 +1,18 @@
+const DUMMY_DATA = [
+    {
+        "name": "shimmerpike",
+        "zone": "auridon",
+        "type": "river",
+        "rarity": "rare",
+        "achievement": ["auridon angler", "dominion fisherman", "master fisher"]
+    },
+    {
+        "name": "mustard eel",
+        "zone": "deshaan",
+        "type": "saltwater",
+        "rarity": "common",
+        "achievement": ["deshaan angler", "pact fisherman", "master fisher"]
+    }
+]
+
+export default DUMMY_DATA
